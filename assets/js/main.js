8127681208843e55f7eab8d885ebe49b618b1f17
ajibola ojo <jibola4jesus@gmail.com>
2020-06-28 22:40:08 +0100

    Remains user review, and app features

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let slider = tns({
+  container: ".my-slider",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+  autoplayButtonOutput: false,
+  loop: true,
+  mouseDrag: true,
+  controls: false,
+  navPosition: "bottom",
+  nav: true,
+});
